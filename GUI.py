e78379b979d592bfe51b1313034560e084ddc9d7
nivyosef12 <nivyosef12@gmail.com>
2022-04-24 21:53:24 +0300

    added mouse and keyboard actions -> choosing a cell and inserting numer (not acording to sudoku rules)

--- GUI.py
+++ GUI.py
@@ -6,7 +6,14 @@ pg.init()
 screen_size = 750, 750
 
 
-def draw_background(screen):
+class Board:
+
+    def __init__(self, board_game):
+        self.board_game = board_game
+        self.selected = (-1, -1)  # selected by mouse click
+
+
+def draw_background(screen, board):
     screen.fill(pg.Color("white"))
     pg.draw.rect(screen, pg.Color("black"), pg.Rect(15, 15, 720, 720), 10)
     i = 1
@@ -19,15 +26,18 @@ def draw_background(screen):
         pg.draw.line(screen, pg.Color("black"), pg.Vector2(15, (i * 80) + 15), pg.Vector2(735, (i * 80) + 15)
                      , line_width)
         i += 1
+        if board.selected != (-1, -1):
+            selected_rect = pg.Rect(15 + (80 * board.selected[0]), 15 + (80 * board.selected[1]), 80, 80)
+            pg.draw.rect(screen, pg.Color("red"), selected_rect, 10)
 
 
-def draw_numbers(screen, font, board):
+def draw_numbers(screen, font, board_game):
     row = 0
     offset = 35
     while row < 9:
         col = 0
         while col < 9:
-            num = board[row][col]
+            num = board_game[row][col]
             if num != 0:
                 n_text = font.render(str(num), True, pg.Color("black"))
                 screen.blit(n_text, pg.Vector2((col * 80) + offset + 5, (row * 80) + offset - 2))
@@ -35,9 +45,9 @@ def draw_numbers(screen, font, board):
         row += 1
 
 
-def draw(screen, font, grid):
-    draw_background(screen)
-    draw_numbers(screen, font, grid)
+def draw(screen, font, board):
+    draw_background(screen, board)
+    draw_numbers(screen, font, board.board_game)
     pg.display.flip()
 
 
@@ -45,35 +55,50 @@ def main():
     screen = pg.display.set_mode(screen_size)
     font = pg.font.SysFont(None, 80)
     pg.display.set_caption("Sudoku")
-    grid = get_grid()
+    board = Board(get_grid())
     key = None
     while 1:
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 sys.exit()
+            if event.type == pg.MOUSEBUTTONUP:
+                pos = pg.mouse.get_pos()
+                if pos[0] < 15 or pos[0] > 735 or pos[1] < 15 or pos[1] > 735:
+                    board.selected = (-1, -1)
+                else:
+                    board.selected = (((pos[0] - 15) // 80), ((pos[1] - 15) // 80))
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_s:
-                    solve(grid)
-                if event.key == pg.K_1:
+                    solve(board.board_game)
+                if event.key == pg.K_1 and board.selected != (-1, -1):
+                    board.board_game[board.selected[1]][board.selected[0]] = 1
                     key = 1
-                if event.key == pg.K_2:
+                if event.key == pg.K_2 and board.selected != (-1, -1):
+                    board.board_game[board.selected[1]][board.selected[0]] = 2
                     key = 2
-                if event.key == pg.K_3:
+                if event.key == pg.K_3 and board.selected != (-1, -1):
+                    board.board_game[board.selected[1]][board.selected[0]] = 3
                     key = 3
-                if event.key == pg.K_4:
+                if event.key == pg.K_4 and board.selected != (-1, -1):
+                    board.board_game[board.selected[1]][board.selected[0]] = 4
                     key = 4
-                if event.key == pg.K_5:
+                if event.key == pg.K_5 and board.selected != (-1, -1):
+                    board.board_game[board.selected[1]][board.selected[0]] = 5
                     key = 5
-                if event.key == pg.K_6:
+                if event.key == pg.K_6 and board.selected != (-1, -1):
+                    board.board_game[board.selected[1]][board.selected[0]] = 6
                     key = 6
-                if event.key == pg.K_7:
+                if event.key == pg.K_7 and board.selected != (-1, -1):
+                    board.board_game[board.selected[1]][board.selected[0]] = 7
                     key = 7
-                if event.key == pg.K_8:
+                if event.key == pg.K_8 and board.selected != (-1, -1):
+                    board.board_game[board.selected[1]][board.selected[0]] = 8
                     key = 8
-                if event.key == pg.K_9:
+                if event.key == pg.K_9 and board.selected != (-1, -1):
+                    board.board_game[board.selected[1]][board.selected[0]] = 9
                     key = 9
-        draw(screen, font, grid)
+        draw(screen, font, board)
 
 
 main()
