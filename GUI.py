cf4f75a469518b07241df8047b02d6150a41f5cc
nivyosef12 <nivyosef12@gmail.com>
2022-04-24 18:34:57 +0300

    combined 2 draw functions int one

--- GUI.py
+++ GUI.py
@@ -1,14 +1,12 @@
 import pygame as pg
 import sys
-from main import get_grid
+from main import get_grid, solve, is_valid_move
 
 pg.init()
 screen_size = 750, 750
-screen = pg.display.set_mode(screen_size)
-font = pg.font.SysFont(None, 80)
 
 
-def draw_background():
+def draw_background(screen):
     screen.fill(pg.Color("white"))
     pg.draw.rect(screen, pg.Color("black"), pg.Rect(15, 15, 720, 720), 10)
     i = 1
@@ -23,7 +21,7 @@ def draw_background():
         i += 1
 
 
-def draw_numbers(board):
+def draw_numbers(screen, font, board):
     row = 0
     offset = 35
     while row < 9:
@@ -37,16 +35,45 @@ def draw_numbers(board):
         row += 1
 
 
+def draw(screen, font, grid):
+    draw_background(screen)
+    draw_numbers(screen, font, grid)
+    pg.display.flip()
+
+
 def main():
-    # in commit -> grid creation takes place at main func, get grid added
+    screen = pg.display.set_mode(screen_size)
+    font = pg.font.SysFont(None, 80)
+    pg.display.set_caption("Sudoku")
     grid = get_grid()
+    key = None
     while 1:
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 sys.exit()
-        draw_background()
-        draw_numbers(grid)
-        pg.display.flip()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_s:
+                    solve(grid)
+                if event.key == pg.K_1:
+                    key = 1
+                if event.key == pg.K_2:
+                    key = 2
+                if event.key == pg.K_3:
+                    key = 3
+                if event.key == pg.K_4:
+                    key = 4
+                if event.key == pg.K_5:
+                    key = 5
+                if event.key == pg.K_6:
+                    key = 6
+                if event.key == pg.K_7:
+                    key = 7
+                if event.key == pg.K_8:
+                    key = 8
+                if event.key == pg.K_9:
+                    key = 9
+        draw(screen, font, grid)
 
 
 main()
